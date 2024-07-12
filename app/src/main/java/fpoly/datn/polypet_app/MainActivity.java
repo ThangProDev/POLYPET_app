388688fe8debb44419b56a1c0cff75f65d2ea906
admin <dangthetien@gmail.com>
2024-07-13 00:57:51 +0700

    Tien comit thêm mainactivity + fragment_favourite

--- app/src/main/java/fpoly/datn/polypet_app/MainActivity.java
+++ app/src/main/java/fpoly/datn/polypet_app/MainActivity.java
@@ -7,18 +7,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import fpoly.datn.polypet_app.adapter.BottomTabNav_Adapter;
+import fpoly.datn.polypet_app.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomTabNav_Adapter adapter = new BottomTabNav_Adapter(this);
+        binding.viewPager.setAdapter(adapter);
+
+        binding.navigation.setOnItemSelectedListener(item -> {
+            int viewPagerPosition = 0; // Default position
+
+            if (item.getItemId() == R.id.btnHome) {
+                viewPagerPosition = 0;
+            } else if (item.getItemId() == R.id.btnFavourite) {
+                viewPagerPosition = 1;
+            } else if (item.getItemId() == R.id.btnNotification) {
+                viewPagerPosition = 2;
+            } else if (item.getItemId() == R.id.btnProfile) {
+                viewPagerPosition = 3;
+            }
+
+            binding.viewPager.setCurrentItem(viewPagerPosition, true);
+            return true;
+        });
+
+        binding.viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                // Update BottomNavigationView when ViewPager2 page changes
+                if (position == 0) {
+                    binding.navigation.setSelectedItemId(R.id.btnHome);
+                } else if (position == 1) {
+                    binding.navigation.setSelectedItemId(R.id.btnFavourite);
+                } else if (position == 2) {
+                    binding.navigation.setSelectedItemId(R.id.btnNotification);
+                } else if (position == 3) {
+                    binding.navigation.setSelectedItemId(R.id.btnProfile);
+                }
+            }
         });
     }
 }
